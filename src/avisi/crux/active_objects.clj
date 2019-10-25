a597be7870d2897a1a1ea6b9a62621fc62dc058f
Timo van der Kamp <t.kamp@avisi.nl>
2019-10-25 11:45:41 +0200

    Single query for checking existing docs in tx
    Resolves: https://github.com/avisi-apps/crux-active-objects/issues/2

--- src/avisi/crux/active_objects.clj
+++ src/avisi/crux/active_objects.clj
@@ -5,7 +5,8 @@
             [crux.tx :as tx]
             [ghostwheel.core :refer [>defn => | <- ?]]
             [clojure.tools.logging :as log]
-            [clojure.edn :as edn])
+            [clojure.edn :as edn]
+            [clojure.string :as str])
   (:import [com.atlassian.activeobjects.external ActiveObjects]
            [java.util Date Map]
            [java.io Closeable]
@@ -18,6 +19,7 @@
 (s/def ::event-log-entry #(instance? EventLogEntry %))
 (s/def ::key (s/nilable (s/and string? #(<= (count %) 100))))
 (s/def ::body any?)
+(s/def ::existing-entries (s/nilable (s/coll-of ::event-log-entry)))
 
 (defn- now ^Date []
   (Date.))
@@ -34,28 +36,35 @@
 (defn create! [^ActiveObjects ao ^Class clz fields]
   (.create ao ^Class clz ^Map fields))
 
+(defn get-existing-event-log-entries [^ActiveObjects ao content-hashes]
+  (seq (.find ao ^Class EventLogEntry
+              (-> (Query/select "ID, BODY, KEY")
+                  (.where (str "KEY IN (" (str/join ", " (repeat (count content-hashes) "?")) ")")
+                          (object-array content-hashes))))))
+
 (>defn save-event-log-entry! ^EventLogEntry
-       [^ActiveObjects ao topic k v]
-       [::ao #{::tx ::doc} ::key ::body => ::event-log-entry]
+       [^ActiveObjects ao topic k v existing-entries]
+       [::ao #{::tx ::doc} ::key ::body ::existing-entries => ::event-log-entry]
        (log/debug "Save event log entry!" {:topic topic
                                            :k k
                                            :v v})
-       (let [body (clj->str v)
-             t ^long (.getTime (now))
-             payload ^Map (cond-> {"BODY" (clj->str v)
-                                   "TOPIC" (name topic)
-                                   "TIME" t}
-                                  k (assoc "KEY" k))]
-         (when k
-           (run!
-            (fn [^EventLogEntry existing]
-              (when (not= (.getBody ^EventLogEntry existing) body)
-                (doto existing
-                  (.setBody body)
-                  (.save))))
-            (seq (.find ao ^Class EventLogEntry (-> (Query/select "ID, BODY")
-                                                    (.where "KEY = ?" (object-array [k])))))))
-         (create! ao EventLogEntry payload)))
+  (let [body (clj->str v)
+        t ^long (.getTime (now))
+        payload ^Map (cond-> {"BODY" (clj->str v)
+                              "TOPIC" (name topic)
+                              "TIME" t}
+                             k (assoc "KEY" k))]
+    (when existing-entries
+      (run!
+       (fn [^EventLogEntry existing]
+         (when (not= (.getBody ^EventLogEntry existing) body)
+           (doto existing
+             (.setBody body)
+             (.save))))
+       existing-entries))
+    (when (or (nil? existing-entries)
+              (tx/evicted-doc? v))
+      (create! ao EventLogEntry payload))))
 
 (defn event-log-entry->crux-tx [^EventLogEntry e]
   {:crux.tx/tx-time (Date. ^long (.getTime e))
@@ -80,17 +89,26 @@
   (add-listener! [this key f])
   (remove-listener! [this key]))
 
-(defrecord ActiveObjectsTxLog [ao listeners]
+(defrecord ActiveObjectsTxLog [^ActiveObjects ao listeners]
   db/TxLog
   (submit-doc [this content-hash doc]
-    (save-event-log-entry! ao ::doc (.toString content-hash) doc))
+    (save-event-log-entry! ao ::doc (.toString content-hash) doc (get-existing-event-log-entries ao [content-hash])))
   (submit-tx [this tx-ops]
     (s/assert :crux.api/tx-ops tx-ops)
-    (doseq [doc (crux.tx/tx-ops->docs tx-ops)]
-      (db/submit-doc this (str (c/new-id doc)) doc))
-    (let [m ^EventLogEntry (save-event-log-entry! ao ::tx nil (tx/tx-ops->tx-events tx-ops))]
-      (delay {:crux.tx/tx-id (.getID m)
-              :crux.tx/tx-time (Date. ^long (.getTime m))})))
+    (let [docs (crux.tx/tx-ops->docs tx-ops)
+          docs-with-hashes (reduce (fn [m doc]
+                                     (conj m [(str (c/new-id doc)) doc]))
+                                   []
+                                   docs)
+          existing (group-by #(.getKey ^EventLogEntry %)
+                             (get-existing-event-log-entries ao (mapv first docs-with-hashes)))]
+      (run! (fn [[hash doc]]
+              (save-event-log-entry! ao ::doc (.toString hash) doc (get existing hash)))
+            docs-with-hashes))
+    (let [m ^EventLogEntry (save-event-log-entry! ao ::tx nil (tx/tx-ops->tx-events tx-ops) nil)]
+      (when m
+        (delay {:crux.tx/tx-id (.getID m)
+                :crux.tx/tx-time (Date. ^long (.getTime m))}))))
   (new-tx-log-context [this]
     (reify Closeable
       (close [this])))
@@ -98,11 +116,11 @@
     (tx-seq ao (or from-tx-id 0)))
   TxListener
   (add-listener! [this k f]
-   (swap! listeners assoc k f)
-   nil)
+    (swap! listeners assoc k f)
+    nil)
   (remove-listener! [this k]
-   (swap! listeners dissoc k)
-   nil)
+    (swap! listeners dissoc k)
+    nil)
   Closeable
   (close [this]
     (.flushAll ao)))
